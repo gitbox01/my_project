b374676b9b58a193646cf2967b70368e690a729c
gitbox01 <niyazrs@mail.ru>
2024-12-26 15:32:42 +0500

    Update hello_git py. 2 - main

--- hello_git.py
+++ hello_git.py
@@ -1,2 +1,9 @@
+print()
 print('*** Hello, Git! ***')
+print()
 print('Индексация - это важно')
+print()
+print('Create new branch "testing"')
+print()
+for num in range(10):
+        print(num + num,end=' ')
