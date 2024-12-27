25f98d5b3397520a380b07a5169d75ee0982b665
SaN <niyazrs@mail.ru>
2024-12-27 13:08:43 +0500

    Update hello_git.py from GitHub

--- hello_git.py
+++ hello_git.py
@@ -8,7 +8,7 @@ ind = 0
 sum = 0
 while ind <= 9:
         sum += ind
-        print(sum)
+        print(sum, sep = '  ')
         ind += 1
 
 print(b.any_func(2, sum))
@@ -17,3 +17,4 @@ print(f'\nYour current directory: {os.getcwd()}')
 s = 'Version Control System'
 print(f'{s} - length is {len(s)} characters')
 
+print('Hello from GitHub!')
